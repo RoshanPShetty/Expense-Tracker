9892e6f042122d8d05be5711526f0882a6405e57
Roshan Shetty <roshanshetty2000@gmail.com>
2021-12-24 03:30:21 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Food",
+      amount: 250.0,
+      date: new Date(2021, 12, 11),
+    },
+    {
+      id: 1,
+      title: "Rent",
+      amount: 575.0,
+      date: new Date(2021, 12, 1),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
